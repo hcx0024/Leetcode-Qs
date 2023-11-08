dc9eff356c4f5d5727c479347d592ca1c4b6b865
hcx0024 <amyhu424323@gmail.com>
2023-11-08 18:20:49 -0500

    update

--- q226_easyBST.cpp
+++ q226_easyBST.cpp
@@ -22,6 +22,15 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
