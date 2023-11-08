4a3a676a0f349432f732d61354a16774411a07d1
hcx0024 <amyhu424323@gmail.com>
2023-11-08 17:40:01 -0500

    104. Maximum Depth of Binary Tree

---
+++ q104_mediumBST.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include<queue>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+/*TAKE AWAY
+    1. recursion, basically devide problem to 2 
+    2. goal is to find max depth, so just need to recursively rind the max of left child and right child
+    3. return 0 is curr node is NULL
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        //recursion
+        if(root == NULL) return 0;
+        return 1+max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
