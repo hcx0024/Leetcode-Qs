01f07c35361264a19fe01afc53e7e233a1939af3
hcx0024 <amyhu424323@gmail.com>
2023-11-08 18:04:40 -0500

    q226 invert trees easy

---
+++ q226_easyBST.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include<queue>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        invertTree(root->left);
+        invertTree(root->right);
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        
+        return root;
+    }
+};
